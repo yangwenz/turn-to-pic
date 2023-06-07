b2cd77ae6214e76ea4b9a8c21763155888af3338
yangwenz <yangwenzhuo08@gmail.com>
2023-06-07 16:05:41 +0800

    First commit

--- components/ExampleCard.tsx
+++ components/ExampleCard.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
+
+let examples = [
+    "test_29.png", "test_42.png", "test_50.png", "test_58.png", "test_66.png", "test_74.png", "test_82.png",
+    "test_32.png", "test_43.png", "test_51.png", "test_59.png", "test_67.png", "test_75.png", "test_83.png",
+    "test_34.png", "test_44.png", "test_52.png", "test_60.png", "test_68.png", "test_76.png", "test_84.png",
+    "test_35.png", "test_45.png", "test_53.png", "test_61.png", "test_69.png", "test_77.png", "test_85.png",
+    "test_37.png", "test_46.png", "test_54.png", "test_62.png", "test_70.png", "test_78.png", "test_86.png",
+    "test_38.png", "test_47.png", "test_55.png", "test_63.png", "test_71.png", "test_79.png", "test_87.png",
+    "test_39.png", "test_48.png", "test_56.png", "test_64.png", "test_72.png", "test_80.png", "test_88.png",
+    "test_41.png", "test_49.png", "test_57.png", "test_65.png", "test_73.png", "test_81.png", "test_89.png"
+]
+
+function shuffle(items: string[]) {
+    for (let i = items.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let x = items[i];
+        items[i] = items[j];
+        items[j] = x;
+    }
+    return items;
+}
 
 export function GalleryExample() {
 
@@ -9,7 +29,7 @@ export function GalleryExample() {
         return (
             <motion.div
                 className="flex flex-col items-center justify-center
-                    lg:w-[256px] w-[160px] lg:h-[256px] h-[160px]
+                    lg:w-[256px] w-[160px] lg:h-[384px] h-[240px]
                     rounded-lg bg-slate-300 overflow-hidden relative"
                 whileHover={{
                     position: 'relative',
@@ -25,24 +45,17 @@ export function GalleryExample() {
         )
     }
 
+    let cards =
+        shuffle(examples).map(img => Card("/images/" + img));
+
     return (
         <div className="rounded-lg shadow-lg m-1 bg-white/25 mb-4 p-3">
             <div
                 className="grid md:grid-cols-4 grid-cols-2"
                 style={{gridGap: "10px"}}
             >
-                {Card("/example_1.webp")}
-                {Card("/example_2.webp")}
-                {Card("/example_3.webp")}
-                {Card("/example_4.webp")}
+                {cards}
             </div>
-            <Link href={"/gallery"} passHref>
-                <button className="bg-gradient-to-br from-purple-500/80 to-blue-500/80
-                    text-white rounded-lg text-small p-2 px-16 flex-none
-                    hover:bg-gradient-to-bl focus:ring-4 mt-3">
-                    Go to Gallery
-                </button>
-            </Link>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import {useSession} from "next-auth/react";
+import {GalleryExample} from "@/components/ExampleCard";
 
 
 export default function Home() {
@@ -31,6 +32,9 @@ export default function Home() {
                         Transform Your Love for Dota 2 into Stunning Visual Art
                     </span>
                 </h2>
+                <div className="lg:flex items-center text-center justify-center">
+                    <GalleryExample/>
+                </div>
             </main>
             <Footer/>
         </div>
