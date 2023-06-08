c8dae9b00e8570bb9b84351c04a985f297489822
yangwenz <yangwenzhuo08@gmail.com>
2023-06-08 17:47:58 +0800

    First commit

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -31,20 +31,22 @@ export default function Settings({showModal, setShowModal}: {
     return (
         <div>
             {showModal ? (
-                <div className="fixed z-50 top-0 left-0 w-screen h-screen bg-gray-800/90">
+                <div className="fixed z-50 top-0 left-0 w-screen h-screen bg-gray-800/50">
                     <div className="fixed z-200 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2
-                        rounded-lg shadow-xl bg-white/80">
-                        <div className="flex items-center justify-center border-b border-gray-600
-                            bg-gradient-to-br from-purple-600 to-blue-500
-                            text-white rounded-t-lg pt-2 pb-2">
+                        rounded-lg shadow-xl bg-gray-900">
+                        <div className="flex items-center justify-center border-b-2 border-slate-500
+                            text-gray-300 rounded-t-lg pt-2 pb-2">
                             <h2 className="text-white text-center text-xl font-bold">
                                 Settings
                             </h2>
                             <FaCog className="text-white text-xl ml-2"/>
                         </div>
+                        <div className="text-gray-300 mt-2 text-left px-4">
+                            Please set your Replicate API key here:
+                        </div>
                         <div className="mt-3 mb-2">
                             <div className="flex m-4">
-                                <div className={"text-black inline-block text-center font-medium pt-2 pb-2 pr-2"}>
+                                <div className={"text-gray-300 inline-block text-center font-bold pt-2 pb-2 pr-2"}>
                                     <p className="inline-block">Replicate API</p>
                                     <FaKey className="inline-block ml-1"/>
                                 </div>
@@ -59,6 +61,9 @@ export default function Settings({showModal, setShowModal}: {
                                 />
                             </div>
                         </div>
+                        <div className="text-gray-300 mb-2 text-left px-4">
+                            Your API key will be encrypted and stored exclusively on your local device
+                        </div>
                         <button
                             className="h-10 px-5 m-2 text-white transition-colors duration-150
                                 bg-gradient-to-br from-gray-500 to-gray-700
@@ -69,7 +74,7 @@ export default function Settings({showModal, setShowModal}: {
                         </button>
                         <button
                             className="h-10 px-5 m-2 text-white transition-colors duration-150
-                                bg-gradient-to-br from-purple-600 to-blue-500
+                                bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700
                                 rounded-lg focus:ring-4 hover:bg-gradient-to-bl"
                             onClick={onClickSave}
                         >
