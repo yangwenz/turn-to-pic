bb7218e18037687cb7b04db454b66830e7c355fe
yangwenz <yangwenzhuo08@gmail.com>
2023-06-09 13:07:56 +0800

    First commit

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -5,6 +5,7 @@ import {
     heroAttributes,
     label2name
 } from "@/configs/heroes";
+import clsx from "clsx";
 
 function Card(label: string, url: string) {
     return (
@@ -25,30 +26,58 @@ export default function HeroCard() {
     const [attribute, setAttribute] = useState<string>("");
     let heroList = attribute === "" ? heroes : heroAttributes.get(attribute)!;
 
+    const clickAttribute = (attr: string) => {
+        if (attribute === attr) {
+            setAttribute("");
+        } else {
+            setAttribute(attr);
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center bg-gray-900 rounded-lg shadow-xl px-2">
-            <div className="flex flex-row items-center justify-center mt-2">
+            <div className="flex flex-row items-center justify-center mt-2 w-full">
                 <select
                     id="herolist"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-                        focus:border-blue-500 block w-full p-2.5"
+                        focus:border-blue-500 block w-1/2 p-2.5"
                 >
                     <option selected>Choose a hero</option>
                     {heroList.map(hero => (<option value={hero} key={hero}>{label2name(hero)}</option>))}
                 </select>
-                <button className="w-auto ml-1">
+                <button
+                    className={clsx(attribute === "strength" ? "bg-gray-500" : "", "w-auto ml-1 rounded")}
+                    onClick={() => {
+                        clickAttribute("strength")
+                    }}
+                >
                     <Image alt="Strength" src="/heroes/Strength_attribute_symbol.png"
                            title="Strength" width={40} height={40}/>
                 </button>
-                <button className="w-auto ml-1">
+                <button
+                    className={clsx(attribute === "agility" ? "bg-gray-500" : "", "w-auto ml-1 rounded")}
+                    onClick={() => {
+                        clickAttribute("agility")
+                    }}
+                >
                     <Image alt="Agility" src="/heroes/Agility_attribute_symbol.png"
                            title="Agility" width={40} height={40}/>
                 </button>
-                <button className="w-auto ml-1">
+                <button
+                    className={clsx(attribute === "intelligence" ? "bg-gray-500" : "", "w-auto ml-1 rounded")}
+                    onClick={() => {
+                        clickAttribute("intelligence")
+                    }}
+                >
                     <Image alt="Intelligence" src="/heroes/Intelligence_attribute_symbol.png"
                            title="Intelligence" width={40} height={40}/>
                 </button>
-                <button className="w-auto ml-1">
+                <button
+                    className={clsx(attribute === "universal" ? "bg-gray-500" : "", "w-auto ml-1 rounded")}
+                    onClick={() => {
+                        clickAttribute("universal")
+                    }}
+                >
                     <Image alt="Universal" src="/heroes/Universal_attribute_symbol.png"
                            title="Universal" width={40} height={40}/>
                 </button>
