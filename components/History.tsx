03a19668d19d9ab49fd3987b41dd747403ca24fd
yangwenz <yangwenzhuo08@gmail.com>
2023-06-12 17:33:37 +0800

    First commit

--- components/History.tsx
+++ components/History.tsx
@@ -1,7 +1,37 @@
 import clsx from "clsx";
 import React from "react";
+import Image from "next/image";
 import {FaBars} from "react-icons/fa";
 import {UserHistoryRecord} from "@/hooks/useHistory";
+
+function HistoryCard(record: UserHistoryRecord) {
+    return (
+        <div className="p-2 rounded-lg bg-gray-700 m-1" key={record.id}>
+            {record.dataUrl && (
+                <div
+                    className="relative rounded-lg overflow-hidden md:w-60 md:h-60 w-48 h-48"
+                >
+                    <Image
+                        src={record.dataUrl}
+                        alt={record.id}
+                        fill
+                        blurDataURL={record.dataUrl}
+                    />
+                </div>
+            )}
+            {!record.dataUrl && (
+                <div
+                    className="relative rounded-lg overflow-hidden md:w-60 md:h-10 w-48 h-8
+                    flex flex-col items-center justify-center"
+                >
+                    <span className="font-semibold italic">
+                        {record.status == "starting"? "Generating ...": "Failed"}
+                    </span>
+                </div>
+            )}
+        </div>
+    )
+}
 
 export default function History({showHistory, setShowHistory, historyRecords}: {
     showHistory: boolean,
@@ -19,7 +49,7 @@ export default function History({showHistory, setShowHistory, historyRecords}: {
                     "fixed top-0 left-full "
                 )}
             >
-                <div className="flex flex-col gap-1 overflow-y-auto">
+                <div className="flex flex-col gap-1 overflow-y-auto items-center">
                     <div className="mb-2 flex justify-center gap-2">
                         <span className="font-bold text-lg">
                             History
@@ -36,7 +66,8 @@ export default function History({showHistory, setShowHistory, historyRecords}: {
                             <FaBars />
                         </button>
                     </div>
-                    <div className="border-b border-slate-500 mt-2 mb-4"></div>
+                    <div className="border-b border-slate-500 mt-2 mb-2"></div>
+                    {historyRecords.slice(0).reverse().map((record) => HistoryCard(record))}
                 </div>
             </div>
         </>
