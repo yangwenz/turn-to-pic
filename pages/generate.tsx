58e15ab9aaf763b98dcd136963cdf9f082528c1e
yangwenz <yangwenzhuo08@gmail.com>
2023-06-19 20:52:52 +0800

    First commit

--- pages/generate.tsx
+++ pages/generate.tsx
@@ -257,10 +257,9 @@ export default function Generate() {
             <div className="flex flex-col lg:max-w-6xl w-full mx-auto items-center min-h-screen">
                 <Header session={session} status={status}/>
                 <main className="flex flex-col items-center justify-center
-                text-center px-4 mt-8 sm:mb-0 mb-auto w-full">
+                    text-center px-4 mt-8 mb-auto w-full">
                     <h1 className="mx-auto font-display text-4xl font-bold
-                    tracking-normal text-slate-900 sm:text-5xl mb-5 drop-shadow-xl"
-                    >
+                        tracking-normal text-slate-900 sm:text-5xl mb-5 drop-shadow-xl">
                     <span className="animate-text bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300
                         bg-clip-text text-transparent font-black">
                         Creating Amazing Pictures of Dota 2 Heroes
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -7,6 +7,7 @@ import {useRouter} from 'next/router';
 import React from "react";
 import {router} from "next/client";
 import { motion } from "framer-motion";
+import DefaultLayout from "@/layout/default";
 
 function Card(url: string) {
     return (
@@ -40,52 +41,50 @@ export default function SignIn() {
     }
 
     return (
-        <div className="flex flex-col max-w-6xl mx-auto items-center min-h-screen">
-            <Head>
-                <title>TurnToPic</title>
-            </Head>
-            <Header session={session} status={status}/>
-            <main
-                className="flex flex-1 w-full flex-col items-center justify-center text-center sm:mb-0 mb-auto">
-                <h1
-                    className="mx-auto font-display text-4xl font-bold tracking-normal text-slate-900 sm:text-5xl mb-5"
-                >
-                    <span className="animate-text bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300
-                        bg-clip-text text-transparent font-black">
-                        Creating Amazing Pictures of Dota 2 Heroes
-                    </span>
-                </h1>
-                <div className="flex flex-col items-center space-y-6 max-w-[750px] mt-4 mb-40">
-                    <div className="max-w-xl text-slate-400 text-xl">
-                        Sign in below to create a free account and make stunning images today.
+        <DefaultLayout>
+            <div className="flex flex-col lg:max-w-6xl w-full mx-auto items-center min-h-screen">
+                <Header session={session} status={status}/>
+                <main className="flex w-full flex-col items-center justify-center
+                        text-center px-4 mt-8 mb-auto">
+                    <h1 className="mx-auto font-display text-4xl font-bold
+                        tracking-normal text-slate-900 sm:text-5xl mb-5 drop-shadow-xl">
+                        <span className="animate-text bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300
+                            bg-clip-text text-transparent font-black">
+                            Creating Amazing Pictures of Dota 2 Heroes
+                        </span>
+                    </h1>
+                    <div className="flex flex-col items-center space-y-6 max-w-[750px] mt-4 mb-40">
+                        <div className="max-w-xl text-slate-400 text-xl">
+                            Sign in below to create a free account and make stunning images today.
+                        </div>
+                        {error && <SignInError error={error}/>}
+                        <button
+                            onClick={() => signIn("google", {callbackUrl: "/"})}
+                            className={buttonStyle}
+                        >
+                            <Image
+                                src="/google.png"
+                                width={25}
+                                height={25}
+                                alt="google's logo"
+                                className="mr-1"
+                            />
+                            <span>Sign in with Google</span>
+                        </button>
                     </div>
-                    {error && <SignInError error={error}/>}
-                    <button
-                        onClick={() => signIn("google", {callbackUrl: "/"})}
-                        className={buttonStyle}
+                    <div
+                        className="grid md:grid-cols-4 grid-cols-2"
+                        style={{gridGap: "10px"}}
                     >
-                        <Image
-                            src="/google.png"
-                            width={25}
-                            height={25}
-                            alt="google's logo"
-                            className="mr-1"
-                        />
-                        <span>Sign in with Google</span>
-                    </button>
-                </div>
-                <div
-                    className="grid md:grid-cols-4 grid-cols-2"
-                    style={{gridGap: "10px"}}
-                >
-                    {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/0.png")}
-                    {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/1.png")}
-                    {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/2.png")}
-                    {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/3.png")}
-                </div>
-                <Footer/>
-            </main>
-        </div>
+                        {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/0.png")}
+                        {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/1.png")}
+                        {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/2.png")}
+                        {Card("https://upcdn.io/12a1yBZ/raw/turn2pic/examples/small/3.png")}
+                    </div>
+                    <Footer/>
+                </main>
+            </div>
+        </DefaultLayout>
     )
 }
 
